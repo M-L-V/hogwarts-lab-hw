8711d23809188881455f6c5d284d46cfd4ff31ad
Talbekk <mt.campbell@outlook.com>
2019-11-06 16:11:19 +0000

    Part 5

---
+++ hogwarts_lab/dumbledore_controller.rb
@@ -0,0 +1,28 @@
+require('sinatra')
+require('sinatra/contrib/all')
+require('pry-byebug')
+
+require_relative('./models/student.rb')
+also_reload('./models/*')
+
+get '/hogwarts' do
+  @students = Student.all()
+  erb(:index)
+end
+
+
+get '/hogwarts/new' do
+  erb(:new)
+end
+
+get '/hogwarts/:id' do
+  @student = Student.find(params[:id])
+  erb(:show)
+end
+
+
+post'/hogwarts' do
+  @student = Student.new(params)
+  @student.save
+  erb(:create)
+end
--- hogwarts_lab/models/student.rb
+++ hogwarts_lab/models/student.rb
@@ -30,9 +30,38 @@ def save()
       @id = student_data.first()['id'].to_i
     end
 
+
+
+  def get_name()
+    return "#{first_name} #{last_name}"
+  end
+
+
+
+
+    def self.all()
+      sql = "SELECT * FROM students"
+      students = SqlRunner.run(sql)
+      result = students.map{ |student| Student.new(student) }
+      return result
+    end
+
     def self.delete_all()
       sql = "DELETE FROM students"
       SqlRunner.run(sql)
     end
+
+    def self.find(id)
+      sql = "SELECT * FROM students WHERE id = $1"
+      values = [id]
+      student = SqlRunner.run(sql, values)
+      result = Student.new(student.first)
+      return result
+    end
+
+
+
+
+
 
 end
